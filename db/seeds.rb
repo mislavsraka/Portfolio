ae8859d1531422ecde3783b0a68ce0dc867e95ca
Mislav Sraka <mislavsraka@gmail.com>
2017-11-04 11:12:16 +0100

    Created seeds file for sample data(final)

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,7 @@
 10.times do |blog|
    Blog.create!(
-   	title: "My Blog Post #{blog}"
-   	body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia 
-   	deserunt mollit anim id est laborum."
+    	title: "My Blog Post #{blog}",
+   		body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut lab"
    	)
 end
 
@@ -10,9 +9,8 @@ puts "10 blog posts created"
 
 5.times do |skill|
 	Skill.create!(
-		title: "Rails #{skill}"
+		title: "Rails #{skill}",
 		percent_utilized: 15
-
 	)
 end
 
@@ -24,8 +22,8 @@ puts "5 skills created"
 		subtitle: "My great service",
 		body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
 		main_image: "http://placehold.it/600x400",
-		thumb_image: "http://placehold.it/350x200",
-
+		thumb_image: "http://placehold.it/350x200"
 		)
 end
+
 puts "9 portfolio items created"
